1bb0588d6dd89338ed20aadd3f5d06115aff6c38
Shelkunov1221 <109527002+Shelkunov1221@users.noreply.github.com>
2022-09-30 18:33:52 +0300

    add project

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
+// burger
+const burger = document?.querySelector('[data-burger]');
+const nav = document?.querySelector('[data-nav]');
+const navItems = nav?.querySelectorAll('a');
+const body = document.body;
+
+
+burger?.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll');
+  burger?.classList.toggle('burger--active');
+  nav?.classList.toggle('nav--visible');
+});
+
+navItems.forEach(el => {
+  el.addEventListener('click', () => {
+    body.classList.remove('stop-scroll');
+  burger?.classList.remove('burger--active');
+  nav?.classList.remove('nav--visible');
+  });
+});
+
+// поиск
+document.addEventListener('DOMContentLoaded', (e) => {
+  document.getElementById('search-open').addEventListener('click', (e) => {
+    document.getElementById('form-search').classList.add('form-search--active')
+  })
+
+  document.getElementById('search-close').addEventListener('click', (e) => {
+    document.getElementById('form-search').classList.remove('form-search--active')
+  })
+
+  document.getElementById('form-search').addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+})
+
+
 document.querySelectorAll(".dropdown__simplebar").forEach(dropdown => {
   new SimpleBar(dropdown, {
-  /* чтобы изначально ползунок был виден */
-  autoHide: false,
-  /* с помощью этого значения вы можете управлять высотой ползунка*/
-  scrollbarMaxSize: 28,
-});
+    /* чтобы изначально ползунок был виден */
+    autoHide: false,
+    /* с помощью этого значения вы можете управлять высотой ползунка*/
+    scrollbarMaxSize: 28,
+  });
 })
 
 
@@ -14,7 +51,7 @@ const activeClassdropdowns = "dropdown__active";
 const activeClassbtns = "btn__active";
 
 btns.forEach(item => {
-  item.addEventListener("click", function() {
+  item.addEventListener('click', function () {
     let DropThis = this.parentElement.querySelector(".dropdown");
     dropdowns.forEach(el => {
       if (el != DropThis) {
@@ -32,12 +69,13 @@ btns.forEach(item => {
 })
 
 
-// свайпер
+// swiper-hero
 const container = document.querySelector(".container")
 const swiper = new Swiper('.swiper', {
   // Default parameters
   slidesPerView: 1,
   spaceBetween: 10,
+  autoHeight: true,
   speed: 2000,
   autoplay: {
     delay: 2000
@@ -58,85 +96,368 @@ const swiper = new Swiper('.swiper', {
 
 // select
 const element = document.querySelector('select');
-    const choices = new Choices(element, {
-      searchEnabled: false,
-      shouldSort: false
-    });
-
-    let ariaLabel = element.getAttribute('aria-label');
-    element.closest('.choises').setAttribute('aria-label', ariaLabel);
-
-  defaultSelect();
+const choices = new Choices(element, {
+  searchEnabled: false,
+  shouldSort: false
+});
 
 
-  document.addEventListener("DOMContentLoaded", () => {
-    let gallerySlider = new Swiper(".slides-container", {
-      slidesPerView: 1,
-      grid: {
-        rows: 1,
-        fill: "row"
-      },
-      spaceBetween: 20,
-      pagination: {
-        el: ".test-section .test-pagination",
-        type: "fraction"
-      },
-      navigation: {
-        nextEl: ".test-next",
-        prevEl: ".test-prev"
-      },
 
-      breakpoints: {
-        441: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        },
+// swiper-gallery
+document.addEventListener("DOMContentLoaded", () => {
+  let gallerySlider = new Swiper(".slides-container", {
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row"
+    },
+    spaceBetween: 20,
+    pagination: {
+      el: ".gallery-section .gallery-pagination",
+      type: "fraction"
+    },
 
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 50
-        }
+    navigation: {
+      nextEl: ".btn-next",
+      prevEl: ".btn-prev"
+    },
+
+    breakpoints: {
+      441: {
+        slidesPerView: 2,
+        spaceBetween: 30
       },
 
-      a11y: false,
-      keyboard: {
-        enabled: true,
-        onlyInViewport: true
-      }, // можно управлять с клавиатуры стрелками влево/вправо
-
-      // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
-      watchSlidesProgress: true,
-      watchSlidesVisibility: true,
-      slideVisibleClass: "slide-visible",
-
-      on: {
-        init: function () {
-          this.slides.forEach((slide) => {
-            if (!slide.classList.contains("slide-visible")) {
-              slide.tabIndex = "-1";
-            } else {
-              slide.tabIndex = "";
-            }
-          });
-        },
-        slideChange: function () {
-          this.slides.forEach((slide) => {
-            if (!slide.classList.contains("slide-visible")) {
-              slide.tabIndex = "-1";
-            } else {
-              slide.tabIndex = "";
-            }
-          });
-        }
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 50
       }
+    },
 
-      // on: {
-      //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
-      //   beforeResize: function () {
-      //     this.slides.forEach((el) => {
-      //       el.style.marginTop = "";
-      //     });
-      //   }
-      // }
-    });
+    a11y: false,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    }, // можно управлять с клавиатуры стрелками влево/вправо
+
+    // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    slideVisibleClass: "slide-visible",
+
+    on: {
+      init: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      },
+      slideChange: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      }
+    }
+
+    // on: {
+    //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+    //   beforeResize: function () {
+    //     this.slides.forEach((el) => {
+    //       el.style.marginTop = "";
+    //     });
+    //   }
+    // }
   });
+
+
+
+// partners-slider
+  let partnersSlider = new Swiper(".partners__slides-container", {
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row"
+    },
+
+    spaceBetween: 20,
+    pagination: {
+      el: ".gallery-section .gallery-pagination",
+      type: "fraction"
+    },
+
+    navigation: {
+      nextEl: ".btn-next",
+      prevEl: ".btn-prev"
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+
+      441: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50
+      },
+
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 50
+      }
+    },
+
+    a11y: false,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    }, // можно управлять с клавиатуры стрелками влево/вправо
+
+    // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    slideVisibleClass: "slide-visible",
+
+    on: {
+      init: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      },
+      slideChange: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      }
+    }
+  });
+
+
+// developments-swiper
+
+  let developmentsSlider = new Swiper(".developments__slides-container", {
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row"
+    },
+    spaceBetween: 20,
+    pagination: {
+      el: ".developments-section .developments-pagination",
+      type: "fraction"
+    },
+    navigation: {
+      nextEl: ".btn-next",
+      prevEl: ".btn-prev"
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+      441: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 50
+      }
+    },
+
+    a11y: false,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    }, // можно управлять с клавиатуры стрелками влево/вправо
+
+    // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    slideVisibleClass: "slide-visible",
+
+    on: {
+      init: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      },
+      slideChange: function () {
+        this.slides.forEach((slide) => {
+          if (!slide.classList.contains("slide-visible")) {
+            slide.tabIndex = "-1";
+          } else {
+            slide.tabIndex = "";
+          }
+        });
+      }
+    }
+  });
+});
+
+
+
+
+$(".accordion").accordion({
+  heightStyle: "content"
+  });
+
+// tabs
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.querySelectorAll('.content__btn').forEach(function(tabsBtn) {
+//     tabsBtn.addEventListener('click', function(event) {
+//       const path = event.currentTarget.dataset.path
+
+//       document.querySelectorAll('.tab-content').forEach(function(tabContent) {
+//         tabContent.classList.remove('tab-content--active')
+//       })
+
+//       document.querySelector(`[data-target="${path}"]`).classList.add('.tab-content--active')
+
+//       document.querySelectorAll('.content__btn').forEach(function(btn) {
+//         btn.classList.remove('content__btn--active')})
+
+//         event.currentTarget.classList.add('content__btn--active')
+//     })
+//   })
+// })
+
+let tabsBtn = document.querySelectorAll('.content__btn');
+let tabsItem = document.querySelectorAll('.tab-content');
+
+tabsBtn.forEach(function(element){
+  element.addEventListener('click', function(e){
+    const path = e.currentTarget.dataset.path;
+
+     tabsBtn.forEach(function(btn){ btn.classList.remove('content__btn--active')});
+     e.currentTarget.classList.add('content__btn--active');
+
+     tabsItem.forEach(function(element){element.classList.remove('tab-content--active')});
+     document.querySelector(`[data-target="${path}"]`).classList.add('tab-content--active');
+  });
+});
+
+// inputmask
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7 (999) 999-99-99");
+
+im.mask(selector);
+
+// validation
+new JustValidate('.form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 30
+    },
+    tel: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector.inputmask.unmaskedvalue()
+        console.log(phone)
+        return Number(phone) && phone.length === 10
+      }
+    },
+    mail: {
+      required: true,
+      email: true
+    },
+  },
+
+  messages: {
+    name: {
+      required: "Вы не ввели имя"
+    },
+    tel: {
+      required: "Вы не ввели телефон",
+      function: "Не достаточное количество символов"
+    }
+  }
+});
+
+// map
+ymaps.ready(init);
+
+let center = [55.760362, 37.614970];
+
+function init() {
+  var map = new ymaps.Map("map", {
+    center: center, // ваши данные
+    zoom: 14
+  });
+
+  let placemark = new ymaps.Placemark(center, {}, {
+    iconLayout: 'default#image',
+    iconImageHref: '../img/location.svg',
+    iconImageSize: [20, 20],
+    iconImageOffset: [0, -30],
+  })
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  map.geoObjects. add(placemark);
+}
+
+ymaps.ready
+
+
+// mask/
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7 (999) 999-99-99");
+
+im.mask(selector);
+
+
+
+
+
+
+
+
+
+
